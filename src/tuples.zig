d072a7d8d60853f3c01cd0ac031a60559682e45a
Matthew Kuo <hi@matthewkuo.com>
2026-06-19 05:30:12 -0700

    Implement normalization, dot product and cross product

--- src/tuples.zig
+++ src/tuples.zig
@@ -73,6 +73,30 @@ pub const Tuple = struct {
         return @sqrt(std.math.pow(f32, self.x, 2) + std.math.pow(f32, self.y, 2) + std.math.pow(f32, self.z, 2));
     }
 
+    pub fn normalize(self: *const Tuple) Tuple {
+        const m: f32 = self.magnitude();
+
+        return .{
+            .x = self.x / m,
+            .y = self.y / m,
+            .z = self.z / m,
+            .w = self.w / m,
+        };
+    }
+
+    pub fn dot(self: *const Tuple, other: *const Tuple) f32 {
+        return self.x * other.x +
+            self.y * other.y +
+            self.z * other.z +
+            self.w * other.w;
+    }
+
+    pub fn cross(self: *const Tuple, other: *const Tuple) Tuple {
+        return Tuple.initVector(self.y * other.z - self.z * other.y,
+                                self.z * other.x - self.x * other.z,
+                                self.x * other.y - self.y * other.x);
+    }
+
     pub fn equals(self: *const Tuple, other: *const Tuple) bool {
         if (@abs(self.x - other.x) > epsilon) return false;
         if (@abs(self.y - other.y) > epsilon) return false;
@@ -226,4 +250,35 @@ test "computing the magnitude of vector(-1, -2, -3)" {
     const expected: f32 = @sqrt(14);
 
     try std.testing.expectEqual(expected, v.magnitude());
+}
+
+test "normalizing vector(4, 0, 0) gives (1, 0, 0)" {
+    const v: Tuple = Tuple.initVector(4, 0, 0);
+    const expected: Tuple = Tuple.initVector(1, 0, 0);
+
+    try std.testing.expectEqual(expected, v.normalize());
+}
+
+test "normalizing vector(1, 2, 3)" {
+    const v: Tuple = Tuple.initVector(1, 2, 3);
+    const expected: Tuple = Tuple.initVector(0.26726, 0.53452, 0.80178);
+
+    try std.testing.expect(expected.equals(&v.normalize()));
+}
+
+test "the dot product of two tuples" {
+    const a: Tuple = Tuple.initVector(1, 2, 3);
+    const b: Tuple = Tuple.initVector(2, 3, 4);
+    const expected: f32 = 20;
+
+    try std.testing.expectEqual(expected, a.dot(&b));
+}
+
+test "the cross product of two vectors" {
+    const a: Tuple = Tuple.initVector(1, 2, 3);
+    const b: Tuple = Tuple.initVector(2, 3, 4);
+
+    try std.testing.expectEqual(Tuple.initVector(-1, 2, -1), a.cross(&b));
+    try std.testing.expectEqual(Tuple.initVector(1, -2, 1), b.cross(&a));
+
 }
